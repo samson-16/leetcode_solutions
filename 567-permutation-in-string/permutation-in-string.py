e6b37aa03632527e88091ccd15974b964297ff8c
Samson <130339249+samson-16@users.noreply.github.com>
2025-02-14 00:12:11 +0300

    Time: 1149 ms (15.85%) | Memory: 18 MB (38.56%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -3,14 +3,19 @@ class Solution:
         s1_counter = Counter(s1)
         k= len(s1)
         l=0
-        win= s2[l:k]
+        win= list(s2[l:k])
         
         w_counter = Counter(win)
         if w_counter == s1_counter:
             return True
         for r in range(k,len(s2)):
+            win.remove(s2[l])
+            w_counter[s2[l]] -=1
+            if w_counter ==0:
+                del w_counter[s2[l]]
             l+=1
-            win= s2[l:r+1]
+            win.append(s2[r])
+    
             w_counter = Counter(win)
             
             if w_counter == s1_counter:
