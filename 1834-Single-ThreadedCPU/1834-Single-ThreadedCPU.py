72083e1845ee97be02c1ee35344574e772e69d80
Samson <130339249+samson-16@users.noreply.github.com>
2025-05-04 13:55:50 -0700

    Create file

---
+++ 1834-Single-ThreadedCPU/1834-Single-ThreadedCPU.py
@@ -0,0 +1,32 @@
+# Last updated: 5/4/2025, 5:53:27 PM
+from heapq import heappush, heappop
+
+class Solution:
+    def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        for i in range(len(tasks)):
+            tasks[i].append(i)  # Add original index
+
+        tasks.sort()  # Sort by enqueue time
+
+        res = []
+        min_heap = []
+        time = 0
+        i = 0
+        n = len(tasks)
+
+        while i < n or min_heap:
+            # If no tasks are available in heap, jump time forward
+            if not min_heap and time < tasks[i][0]:
+                time = tasks[i][0]
+
+            # Add all available tasks up to current time
+            while i < n and tasks[i][0] <= time:
+                heappush(min_heap, (tasks[i][1], tasks[i][2]))  # (processingTime, index)
+                i += 1
+
+            if min_heap:
+                pt, idx = heappop(min_heap)
+                time += pt
+                res.append(idx)
+
+        return res
