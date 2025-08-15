9699d0d3c9aa694c9697db853bb49cd27a18ddb1
Samson <130339249+samson-16@users.noreply.github.com>
2025-08-15 07:19:48 +0300

    Create 371-SumofTwoIntegers

---
+++ 371-SumofTwoIntegers/version/python/371-SumofTwoIntegers_20250815_071955.py
@@ -0,0 +1,16 @@
+# Last updated: 8/15/2025, 7:19:55 AM
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF
+        MAX_INT = 0x7FFFFFFF
+
+        a =a&mask
+        b = b & mask
+
+        while b:
+            carry = (a & b) <<1
+            a = (a^ b) & mask
+            b = carry  & mask
+        return a if a <=MAX_INT else ~(a^mask)
+
+
