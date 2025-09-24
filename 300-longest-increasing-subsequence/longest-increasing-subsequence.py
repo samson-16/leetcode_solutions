888284c1d4219b7236286ccadf117e43a142114a
Samson <130339249+samson-16@users.noreply.github.com>
2025-09-24 10:13:26 +0300

    Time: 1579 ms (64.68%) | Memory: 18.3 MB (18.91%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,24 +1,19 @@
-
-__import__("atexit").register(lambda: open("display_runtime.txt","w").write("0"))
-
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-        arr = []
-        arr.append((nums[0],1))
+        arr = [1] * n
+     
         for i in range(1,n):
             max_val =0
-            for k in range(len(arr)):
-                val = arr[k][0]
-                num = arr[k][1]
-                if val < nums[i]:
-                    max_val = max(max_val, num)
+            for k in range(i):
+               
+                if nums[k] < nums[i]:
+                    max_val = max(max_val,arr[k])
             
-            arr.append((nums[i],max_val+1))
-        max_num =0
-        for _, num in arr:
-            max_num = max(max_num, num)
-        return max_num
+            arr[i]= max_val+1
+        
+      
+        return max(arr)
             
             
             
