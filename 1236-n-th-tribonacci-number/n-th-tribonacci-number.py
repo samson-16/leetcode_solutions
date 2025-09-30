a65b5c1fd6c21d9e0db51e16a51bd9ffeb7700ee
Samson <130339249+samson-16@users.noreply.github.com>
2025-09-30 11:58:07 +0300

    Time: 0 ms (100.00%) | Memory: 17.9 MB (29.54%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,20 +1,16 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        ans =0
-        @cache
-        def dp(i):
-            nonlocal ans
-            if i==0:
-                return 0
-            if i==1:
-                return 1
-            if i==2:
-                return 1
-            
-            return dp(i-1)+dp(i-2)+dp(i-3)
-            # return ans
+        
+        dp = [0]*(n+1)
+        if n==0:
+            return 0
+        elif n<=2:
+            return 1
+        dp[1], dp[2] = 1,1
+        for i in range(3, n+1):
+            dp[i]=dp[i-1]+dp[i-2]+dp[i-3]
+        return dp[n]
 
-        return dp(n)
 
 
         
