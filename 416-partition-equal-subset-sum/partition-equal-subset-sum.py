7170a3cf816ce7e9a7d8d83558b8505903f00611
Samson <130339249+samson-16@users.noreply.github.com>
2025-10-01 14:38:32 +0300

    Time: 1614 ms (28.39%) | Memory: 33.1 MB (44.27%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,32 +1,26 @@
-__import__("atexit").register(lambda: open("display_runtime.txt","w").write("0"))
-
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        memo ={}
-        n = len(nums)
-        total = sum(nums)
-        if total % 2 ==1:
+        tot=sum(nums)
+        if tot%2!=0:
             return False
-        half = total//2
-
-        def dp(end, weight):
-
-            if weight > half:
-                return False
-            if weight == half:
-                return True
         
-            if end < 0:
-                return False
-            if (end, weight) in memo:
-                return memo[(end, weight)]
-            # cur = weight + nums[end]
-            # take  = dp(end-1, cur)
+        half=tot//2
+        n=len(nums)
+        dp=[[False]*(half+1) for _ in range(n+1)]
+        for i in range(n+1):
+            dp[i][0]=True
 
-            # notake = dp(end-1,weight)
+        for i in range(1, n+1):
+            for w in range(1,half+1):
+                if w < nums[i-1]:
+                    dp[i][w]=dp[i-1][w]
+                else:
 
-            memo[(end, weight)] = (dp(end-1, weight + nums[end]) or dp(end-1, weight))
+                    dp[i][w] = dp[i-1][w-nums[i-1]] or dp[i-1][w]
+        return dp[n][half]
 
-            return memo[(end, weight)]
-        return dp(n-1, 0)
+
+
+
+
 
