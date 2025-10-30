7c1aa7d089d58eb9b1685954375aa523325887e4
Samson <130339249+samson-16@users.noreply.github.com>
2025-10-30 17:50:24 +0300

    Time: 59 ms (24.79%) | Memory: 19 MB (17.55%) - LeetSync

--- 720-longest-word-in-dictionary/longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/longest-word-in-dictionary.py
@@ -1,21 +1,48 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}   
+        self.is_end = False  
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        """Insert a word into the trie."""
+        node = self.root
+        for ch in word:
+            if ch not in node.children:
+                node.children[ch] = TrieNode()
+            node = node.children[ch]
+        node.is_end = True
+
+    def search(self, word: str):
+        flag = True
+        node = self.root
+        for ch in word:
+            node = node.children[ch]
+            if not node.is_end:
+                flag = False
+                break
+        return word if flag else ""
+
 
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        # trie = Trie()
+        trie = Trie()
         words.sort()
-        word_set = set(words)
-        print(word_set)
-        ans = ""
         for word in words:
-            flag = True
-            for i in range(1,len(word)):
-                if word[:i] not in word_set:
-                    flag = False
-                    break
-            if flag:
-                if len(word) > len(ans) or (ans and word < ans):
-                    ans = word
-        return ans            
+            trie.insert(word)
+        ans  = ''
+        for word in words:
+            temp = trie.search(word)
+           
+            if len(temp) > len(ans):
+                ans =  temp
+        return ans
+        
+
+
+
 
         
 
