a8e3f373304c3f802b888b9385ef7b30ec9d64c6
Samson <130339249+samson-16@users.noreply.github.com>
2025-11-01 14:38:16 +0300

    Time: 95 ms (82.73%) | Memory: 23.6 MB (58.96%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,16 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        memo = {}
+        cash = 0
+        hold = -prices[0]
 
-        def dp(end, state):
-            if end == len(prices):
-                return 0
-            if (end, state) in memo:
-                return memo[(end, state)]
-            
-            if state == "hold":
-                memo[(end, state)] = max((dp(end+1,"free")+prices[end]), dp(end+1, "hold"))
-            else:
-                memo[(end, state)] = max((dp(end+1, "hold")-prices[end]-fee), (dp(end+1,'free')))
-            return memo[(end, state)]
-        return dp(0,"free")
+        for price in prices[1:]:
+            new_cash = max(cash, hold + price - fee)
+            new_hold = max(hold, cash - price)
+            cash, hold = new_cash, new_hold
+
+        return cash
